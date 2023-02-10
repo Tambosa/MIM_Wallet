d70aea7f38d46d1141568d2a3e02dadf72ba6773
Tambosa <tambosa@gmail.com>
2023-02-10 15:52:57 +0300

    compose feature: Header

--- app/src/main/java/com/aroman/mimwallet/presentation_compose/ComposeActivity.kt
+++ app/src/main/java/com/aroman/mimwallet/presentation_compose/ComposeActivity.kt
@@ -1,13 +1,21 @@
 package com.aroman.mimwallet.presentation_compose
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.aroman.mimwallet.R
 import com.aroman.mimwallet.presentation_compose.ui.theme.AppTheme
+import com.aroman.mimwallet.presentation_compose.ui.theme.Typography
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -16,8 +24,11 @@ class ComposeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AppTheme {
-                Surface {
-                    Greeting("World")
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.primaryContainer
+                ) {
+                    Header()
                 }
             }
         }
@@ -25,14 +36,47 @@ class ComposeActivity : AppCompatActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Header() {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+
+        val context = LocalContext.current
+
+        Text(
+            modifier = Modifier.padding(start = 12.dp, top = 20.dp),
+            text = "Portfolio",
+            color = MaterialTheme.colorScheme.onPrimaryContainer,
+            style = Typography.titleLarge
+        )
+        IconButton(
+            modifier = Modifier.padding(end = 12.dp, top = 20.dp),
+            onClick = {
+                Toast.makeText(context, "Night mode clicked", Toast.LENGTH_SHORT)
+                    .show()
+            }) {
+            Icon(
+                painter = if (!isSystemInDarkTheme()) painterResource(id = R.drawable.ic_baseline_nights_stay_24)
+                else painterResource(id = R.drawable.ic_baseline_wb_sunny_24),
+                tint = MaterialTheme.colorScheme.onPrimaryContainer,
+                contentDescription = "toggle dark mode"
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     AppTheme {
-        Greeting("World")
+        Surface(
+            modifier = Modifier
+                .width(IntrinsicSize.Max)
+                .height(IntrinsicSize.Max),
+            color = MaterialTheme.colorScheme.primaryContainer
+        ) {
+            Header()
+        }
     }
 }
--- app/src/main/java/com/aroman/mimwallet/presentation_compose/ui/theme/Type.kt
+++ app/src/main/java/com/aroman/mimwallet/presentation_compose/ui/theme/Type.kt
@@ -2,14 +2,18 @@ package com.aroman.mimwallet.presentation_compose.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.aroman.mimwallet.R
 
 val Typography = Typography(
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
+    titleLarge = TextStyle(
+        fontFamily = FontFamily(
+            Font(R.font.inter)
+        ),
+        fontWeight = FontWeight.Bold,
+        fontSize = 36.sp
     ),
 )
