853e5c4604ce18503a27bc5dc6e87c7e8fafd9bb
Tambosa <tambosa@gmail.com>
2023-03-22 14:13:35 +0300

    app icon + splash v31

--- app/src/main/java/com/aroman/mimwallet/presentation_compose/ComposeActivity.kt
+++ app/src/main/java/com/aroman/mimwallet/presentation_compose/ComposeActivity.kt
@@ -1,11 +1,14 @@
 package com.aroman.mimwallet.presentation_compose
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.collectAsState
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -16,13 +19,25 @@ import com.aroman.mimwallet.presentation_compose.ui.theme.AppTheme
 import com.aroman.mimwallet.presentation_compose.ui.viewmodels.ComposeThemeViewModel
 import com.aroman.mimwallet.presentation_compose.ui.viewmodels.ComposeWalletViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
 
 @AndroidEntryPoint
 class ComposeActivity : AppCompatActivity() {
     private val themeViewModel by viewModels<ComposeThemeViewModel>()
     private val walletViewModel by viewModels<ComposeWalletViewModel>()
+
+    private var keepSplash = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val splashScreen = installSplashScreen()
+            splashScreen.setKeepOnScreenCondition { keepSplash }
+            lifecycleScope.launchWhenCreated {
+                delay(1000)
+                keepSplash = false
+            }
+        }
         setContent {
             val isDarkTheme =
                 themeViewModel.isDarkTheme.collectAsState(initial = isSystemInDarkTheme())
