2f9d68c5e285c291deb87491f3eb1c3d7829d296
Tambosa <tambosa@gmail.com>
2023-05-04 15:50:40 +0300

    changed notificationsScreen animations

--- app/src/main/java/com/aroman/mimwallet/presentation/ui/portfolio_notifications/PortfolioNotificationsScreen.kt
+++ app/src/main/java/com/aroman/mimwallet/presentation/ui/portfolio_notifications/PortfolioNotificationsScreen.kt
@@ -5,17 +5,23 @@ import android.content.Context
 import android.os.Build
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.animation.core.LinearOutSlowInEasing
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.tween
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.derivedStateOf
@@ -25,82 +31,103 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import com.aroman.mimwallet.R
 import com.aroman.mimwallet.data.feature_notifications.PortfolioNotificationManager.CHANNEL_ID
-import com.aroman.mimwallet.domain.model.NoticePortfolioUiState
 import com.aroman.mimwallet.domain.model.NoticePortfolioUiEvent
+import com.aroman.mimwallet.domain.model.NoticePortfolioUiState
 import com.aroman.mimwallet.presentation.ui.portfolio_notifications.components.DisplayableInsertNoticePortfolio
 import com.aroman.mimwallet.presentation.ui.portfolio_notifications.components.DisplayableNoticePortfolioItem
-import com.aroman.mimwallet.presentation.ui.portfolio_notifications.components.NotificationsTitle
-import com.aroman.mimwallet.presentation.ui.portfolio_notifications.components.PortfolioNotificationsHeader
+import com.aroman.mimwallet.presentation.ui.portfolio_notifications.components.NextNotificationTimer
 import com.aroman.mimwallet.utils.createNotificationChannel
 import com.aroman.mimwallet.utils.isNotificationAllowed
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PortfolioNotificationsScreen(
     noticePortfolioUiState: NoticePortfolioUiState,
     onEvent: (NoticePortfolioUiEvent) -> Unit
 ) {
+    LaunchedEffect(Unit) {
+        onEvent(NoticePortfolioUiEvent.ShowData(noticePortfolioUiState))
+    }
+
     val context = LocalContext.current
     var hasNotificationPermission by remember(isNotificationAllowed(context))
     InitNotificationPermissions(context, hasNotificationPermission) {
         hasNotificationPermission = it
     }
-    val lazyListState = rememberLazyListState()
-    val isFirstItemVisible by remember {
-        derivedStateOf {
-            val layoutInfo = lazyListState.layoutInfo
-            val visibleItemsInfo = layoutInfo.visibleItemsInfo
-            if (layoutInfo.totalItemsCount == 0) {
-                true
-            } else {
-                visibleItemsInfo.first().index == 0
-            }
-        }
-    }
-    val cardAlpha by animateFloatAsState(
-        targetValue = if (isFirstItemVisible) 1f else 0f, animationSpec = tween(
-            durationMillis = 300, easing = LinearOutSlowInEasing
-        )
-    )
-    LaunchedEffect(Unit) {
-        onEvent(NoticePortfolioUiEvent.ShowData(noticePortfolioUiState))
-    }
+    val listState = rememberLazyListState()
+    val isScrolled by remember { derivedStateOf { listState.firstVisibleItemIndex > 0 } }
+
     Surface(
         modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.primaryContainer
     ) {
-        if (noticePortfolioUiState.noticeList.isEmpty()) {
-            Column(
-                modifier = Modifier.fillMaxWidth(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                NotificationsTitle()
-                DisplayableInsertNoticePortfolio(onEvent)
-            }
-        } else {
-            Column {
-                PortfolioNotificationsHeader(
-                    nextTimerInMillis = noticePortfolioUiState.nextTimerInMillis,
-                    isExpanded = isFirstItemVisible,
-                    cardAlpha = cardAlpha
-                )
-                LazyColumn(
-                    modifier = Modifier.fillMaxSize(),
-                    state = lazyListState,
-                    verticalArrangement = Arrangement.Center
-                ) {
-                    items(count = noticePortfolioUiState.noticeList.size,
-                        key = { index -> noticePortfolioUiState.noticeList[index].id },
-                        itemContent = { index ->
-                            DisplayableNoticePortfolioItem(
-                                noticePortfolioUiState.noticeList[index],
-                                onEvent
+        Scaffold(
+            modifier = Modifier.background(MaterialTheme.colorScheme.primaryContainer),
+            topBar = {
+                TopAppBar(
+                    colors = TopAppBarDefaults.largeTopAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.primaryContainer
+                    ),
+                    title = {
+                        Column(modifier = Modifier.fillMaxWidth()) {
+                            Text(
+                                text = stringResource(id = R.string.portfolio_notifications),
+                                style = MaterialTheme.typography.titleMedium,
+                                color = MaterialTheme.colorScheme.onPrimaryContainer,
+                                modifier = Modifier
+                                    .background(MaterialTheme.colorScheme.primaryContainer)
                             )
-                            if (index == noticePortfolioUiState.noticeList.size - 1) {
-                                DisplayableInsertNoticePortfolio(onEvent)
-                            }
-                        })
+                        }
+                    },
+                    modifier = Modifier.background(MaterialTheme.colorScheme.primaryContainer),
+                )
+            }
+        ) { innerPadding ->
+            if (noticePortfolioUiState.noticeList.isEmpty()) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(top = innerPadding.calculateTopPadding())
+                        .background(MaterialTheme.colorScheme.primaryContainer),
+                    contentAlignment = Alignment.Center
+                ) {
+                    DisplayableInsertNoticePortfolio(onEvent = onEvent)
                 }
+            } else {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = innerPadding.calculateTopPadding())
+                        .background(MaterialTheme.colorScheme.primaryContainer)
+                ) {
+                    AnimatedVisibility(noticePortfolioUiState.nextTimerInMillis != null && !isScrolled) {
+                        NextNotificationTimer(
+                            nextTimerInMillis = noticePortfolioUiState.nextTimerInMillis ?: 0
+                        )
+                    }
+                    LazyColumn(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(MaterialTheme.colorScheme.primaryContainer),
+                        state = listState,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        items(count = noticePortfolioUiState.noticeList.size,
+                            key = { index -> noticePortfolioUiState.noticeList[index].id },
+                            itemContent = { index ->
+                                DisplayableNoticePortfolioItem(
+                                    noticePortfolioUiState.noticeList[index],
+                                    onEvent
+                                )
+                                if (index == noticePortfolioUiState.noticeList.size - 1) {
+                                    DisplayableInsertNoticePortfolio(onEvent)
+                                }
+                            })
+                    }
+                }
+
             }
         }
     }
--- app/src/main/java/com/aroman/mimwallet/presentation/ui/portfolio_notifications/components/NextNotificationTimer.kt
+++ app/src/main/java/com/aroman/mimwallet/presentation/ui/portfolio_notifications/components/NextNotificationTimer.kt
@@ -3,12 +3,14 @@ package com.aroman.mimwallet.presentation.ui.portfolio_notifications.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.aroman.mimwallet.R
 import java.util.concurrent.TimeUnit
@@ -24,13 +26,15 @@ fun NextNotificationTimer(nextTimerInMillis: Long) {
     Row(
         modifier = Modifier
             .background(MaterialTheme.colorScheme.primaryContainer)
-            .padding(bottom = 8.dp),
+            .fillMaxWidth()
+            .padding(top = 8.dp ,bottom = 12.dp),
         horizontalArrangement = Arrangement.Center
     ) {
         Text(
             text = notificationText,
-            style = MaterialTheme.typography.bodyMedium,
-            color = MaterialTheme.colorScheme.onPrimaryContainer
+            style = MaterialTheme.typography.bodyLarge,
+            color = MaterialTheme.colorScheme.onPrimaryContainer,
+            textAlign = TextAlign.Center
         )
     }
 }
